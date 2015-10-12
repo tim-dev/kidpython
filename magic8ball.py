14eff701b59d1d57cc27487801fcd8cce256c6c7
Tim Brown <tim.brown.dev@gmail.com>
2015-10-11 18:11:57 -0700

    first commit

---
+++ magic8ball.py
@@ -0,0 +1,32 @@
+import random
+
+
+options = ["thing1", "thing2", "thing3"]
+
+
+options = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+]
+while True:
+    raw_input("What is your question?\nQ: ")
+    answer = random.choice(options)
+    print("A: " + answer + "\n")
